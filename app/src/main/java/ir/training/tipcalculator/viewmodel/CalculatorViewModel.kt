6f0d97fb00b14619c835d06b52cbe0eb1d13dc3e
Mohammad Javad <mj.alijani@gmail.com>
2021-04-14 17:53:41 +0430

    binding view input with BaseObservable

--- app/src/main/java/ir/training/tipcalculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/ir/training/tipcalculator/viewmodel/CalculatorViewModel.kt
@@ -2,10 +2,11 @@ package ir.training.tipcalculator.viewmodel
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.databinding.BaseObservable
 import ir.training.tipcalculator.model.Calculator
 import ir.training.tipcalculator.model.TipCalculation
 
-class CalculatorViewModel(val calculator: Calculator = Calculator()) {
+class CalculatorViewModel(val calculator: Calculator = Calculator()) : BaseObservable() {
     var inputCheckAmount = ""
     var inputTipPercentage = ""
     var tipCalculation = TipCalculation()
@@ -16,9 +17,17 @@ class CalculatorViewModel(val calculator: Calculator = Calculator()) {
         val tipPct = inputTipPercentage.toIntOrNull()
 
         if (checkAmount != null && tipPct != null) {
+            Log.d(TAG, "checkAmount: $checkAmount & tipPercentage: $tipPct")
             tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            clearInputs()
         }
 
+    }
+
+    private fun clearInputs() {
+        inputCheckAmount = ""
+        inputTipPercentage = ""
+        notifyChange()
     }
 
 }
